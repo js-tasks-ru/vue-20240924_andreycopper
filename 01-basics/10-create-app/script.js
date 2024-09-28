04cf428601d621b271e56237163e4e26e6c13fd2
andreycopper <andreycopper@gmail.com>
2024-09-28 12:27:54 +0500

    Basic. Part 1

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,1 +1,20 @@
 import { defineComponent, createApp } from 'vue'
+
+const App = defineComponent({
+  name: 'App',
+
+  setup() {
+    function currenDate() {
+      return new Date().toLocaleDateString(navigator.language, { dateStyle: 'long' });
+    }
+
+    return {
+      currenDate
+    }
+  },
+
+  template: '<div>Сегодня {{ currenDate() }}</div>'
+});
+
+const app = createApp(App);
+app.mount('#app');
