bfa9f23f7f165c572f70cb61e79044c51678bb33
andreycopper <andreycopper@gmail.com>
2024-10-05 13:47:22 +0500

    UiClock

--- 03-components/20-UiClock/UiClock.js
+++ 03-components/20-UiClock/UiClock.js
@@ -1,9 +1,25 @@
-import { defineComponent } from 'vue'
+import {computed, defineComponent, onUnmounted, ref} from 'vue'
 
 export default defineComponent({
   name: 'UiClock',
 
-  setup() {},
+  setup() {
+    const date = ref(new Date());
+    const time = computed(() => date.value.toLocaleTimeString(
+      navigator.language,
+      { timeStyle: 'medium' }
+    ));
 
-  template: `<div class="clock">10:12:02</div>`,
-})
+    let intervalId = setInterval(() => date.value = new Date(), 1000);
+
+    onUnmounted(() => {
+      clearInterval(intervalId);
+    });
+
+    return {
+      time,
+    }
+  },
+
+  template: `<div class="clock">{{ time }}</div>`,
+});
